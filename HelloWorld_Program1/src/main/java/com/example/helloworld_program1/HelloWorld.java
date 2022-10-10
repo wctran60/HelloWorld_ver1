99238db6bf6a3645bb1e95d94d2caccd73f255b8
wctran60 <114837885+wctran60@users.noreply.github.com>
2022-10-09 23:31:51 -0400

    add William to printout2

--- HelloWorld_Program1/src/main/java/com/example/helloworld_program1/HelloWorld.java
+++ HelloWorld_Program1/src/main/java/com/example/helloworld_program1/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!  This is main class");
         System.out.println("Hello world!  This is test branch 2");
+        William myWilliam = new William();
+        myWilliam.message();
+
     }
 }
