a082ff0f1ccbcaa59e114272f42bcc0f0bce21f7
wctra <wctran@yahoo.com>
2022-10-21 01:05:39 -0400

    test line 1 with three Again

--- HelloWorld_Program1/src/main/java/com/example/helloworld_program1/HelloWorld.java
+++ HelloWorld_Program1/src/main/java/com/example/helloworld_program1/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example.helloworld_program1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!  This is a test.  need to discard after use again AND AGAIN");
+        System.out.println("Hello world!  This is a test.  need to discard after use again AND AGAIN and again");
         System.out.println("Hello world!  This is main class");
         System.out.println("Hello world!  This is test branch 2");
         William myWilliam = new William();
